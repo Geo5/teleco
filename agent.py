0324eb6b31398f98b3f077b8ebd4e1c729a2a3fb
VolodinaAlona <147334674+VolodinaAlona@users.noreply.github.com>
2024-03-20 20:24:04 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -2,21 +2,29 @@
 
 from __future__ import annotations
 
+"""імпортує модуль socket, який дозволяє використовувати функціональність для мережевого програмування, 
+зокрема для створення, з'єднання та обміну даними через сокети."""
 import socket
 import sys
+"""дозволяє обробляти аргументи командного рядка в програмі"""
 from argparse import ArgumentParser
+"""дозволяє створювати класи з автоматично згенерованими методами"""
 from dataclasses import dataclass
+"""імпортує клас IntEnum з модуля enum, який дозволяє створювати переліки з цілими значеннями"""
 from enum import IntEnum
-from typing import Self, TypeVar, cast
+"""імпортує деякі об'єкти з модуля typing"""
+from typing import Self, TypeVar, cast 
 
 
 class Asn1DataType(IntEnum):
     """Asn1 data type."""
+    """ Перелік, що містить різні типи даних Asn1"""
 
     INTEGER = 0x02
     OCTET_STRING = 0x04
     NULL_asn1 = 0x05
     OBJECT_IDENTIFIER = 0x06
+    """тип даних ASN.1 для послідовностей (структур)"""
     SEQUENCE = 0x30
     IpAddress = 0x40
     Gauge32 = 0x42
@@ -28,7 +36,7 @@ class Asn1DataType(IntEnum):
 
 class MaxAccess(IntEnum):
     """Object access permission."""
-
+"""Перелік, який визначає рівні доступу до об'єктів - not_accessible, read_only, read_write"""
     not_accessible = 0
     read_only = 1
     read_write = 2
@@ -36,7 +44,7 @@ class MaxAccess(IntEnum):
 
 class ObjectType(IntEnum):
     """Managed object type."""
-
+"""Перелік, що визначає типи об'єктів(перелік, позначення таблиці, позначення рядків та стовпців таблиці"""
     scalar = 0
     table = 1
     row = 2
@@ -45,13 +53,13 @@ class ObjectType(IntEnum):
 
 class ErrorStatus(IntEnum):
     """SNMP message error status."""
-
-    noError = 0
-    tooBig = 1
-    noSuchName = 2
-    badValue = 3
-    readOnly = 4
-    genErr = 5
+"""Перелік, що визначає різні статуси помилок у SNMP повідомленнях"""
+    noError = 0 """вказує на відсутність помилок у SNMP повідомленні"""
+    tooBig = 1  """розмір повідомлення перевищує максимально допустимий"""
+    noSuchName = 2 """вказане ім'я не існує"""
+    badValue = 3 """передане значення недопустиме або неправильне."""
+    readOnly = 4 """об'єкт доступний лише для читання"""
+    genErr = 5 """загальна помилка"""
 
 
 @dataclass
@@ -60,7 +68,7 @@ class VariableBind:
 
     Type of value depends on data_type.
     """
-
+"""Клас, який представляє зв'язки змінних в SNMP повідомленнях. Містить OID (Object Identifier), тип даних та значення"""
     oid: str
     data_type: Asn1DataType
     value: int | bytes | str | None
@@ -72,7 +80,7 @@ class Object:
 
     Type of value depends on data_type.
     """
-
+"""Клас, який представляє керовані об'єкти у системі(Містить тип даних, значення, тип об'єкту та рівень доступу)"""
     data_type: Asn1DataType
     value: int | bytes | str | None
     object_type: ObjectType = ObjectType.scalar
@@ -80,6 +88,7 @@ class Object:
 
 
 def decode_tlv(buffer: bytes) -> tuple[bytes, Asn1DataType, bytes | None]:
+    """Функція для декодування TLV (Type-Length-Value) структури з буфера байтів."""
     """Decode on tlv encoded value from a buffer of bytes.
 
     Args:
@@ -104,12 +113,13 @@ def decode_tlv(buffer: bytes) -> tuple[bytes, Asn1DataType, bytes | None]:
         raise ValueError(msg)
     # Check if have a context specific tag
     if buffer[0] & 0b1000_0000:
-        # Check if we have a constructed tag (can contain other values)
+        # Check if we have a constructed tag (can contain other values)  Перевірте, чи є у нас створений тег (може містити інші значення)
         data_type = Asn1DataType.CHOICE
         # Add the concrete choice type as attribute
         data_type.tag = buffer[0] & 0b0001_111
         if not (buffer[0] & 0b0010_0000):
             # We do not need to decode the length, if we don't have a constructed tag
+            # Нам не потрібно декодувати довжину, якщо ми не маємо створеного тегу
             return buffer[2:], data_type, None
     else:
         # Decode type
@@ -126,11 +136,12 @@ def decode_tlv(buffer: bytes) -> tuple[bytes, Asn1DataType, bytes | None]:
 
     return buffer[2 + length :], data_type, buffer[2 : 2 + length]
 
-
+"""Функція для кодування значення в TLV форматі за заданим типом даних."""
 def decode_tlv_value(
     buffer: bytes,
 ) -> tuple[bytes, Asn1DataType, int | str | bytes | None]:
     """Like decode_tlv but tries to decode the resulting value to a concrete type."""
+    """Подібно до decode_tlv, але намагається декодувати отримане значення до конкретного типу"""
     buffer, data_type, raw_value = decode_tlv(buffer)
     if raw_value is None:
         return buffer, data_type, raw_value
@@ -153,15 +164,15 @@ def read_tlv_int(buffer: bytes) -> tuple[bytes, int]:
 
     Args:
     ----
-        buffer: Buffer of bytes to read from.
+        buffer: Buffer of bytes to read from.(Буфер байтів для читання)
 
     Raises:
     ------
-        ValueError: If value is not an integer.
+        ValueError: If value is not an integer.(Якщо значення не є цілим числом)
 
     Returns:
     -------
-        The remaining (unread) buffer, the decoded integer.
+        The remaining (unread) buffer, the decoded integer.(Буфер, що залишився (непрочитаний), декодоване ціле число)
 
     """
     buffer, data_type, value = decode_tlv(buffer)
@@ -198,7 +209,7 @@ def read_tlv_string(buffer: bytes) -> tuple[bytes, str]:
 
 def read_tlv_sequence(buffer: bytes) -> tuple[bytes, bytes]:
     """Read a sequence and its contents.
-
+            Прочитайте послідовність і її зміст.
     Args:
     ----
         buffer: Buffer of bytes to read from.
@@ -206,7 +217,7 @@ def read_tlv_sequence(buffer: bytes) -> tuple[bytes, bytes]:
     Returns:
     -------
         The remaining (unread) buffer, the contents of the sequence
-
+            Буфер, що залишився (непрочитаний), вміст послідовності
     """
     buffer, data_type, value = decode_tlv(buffer)
     if data_type != Asn1DataType.SEQUENCE or value is None:
@@ -221,11 +232,11 @@ CT = TypeVar("CT", bound=IntEnum)
 
 def read_tlv_choice(buffer: bytes, choices: type[CT]) -> tuple[bytes, CT, bytes | None]:
     """Read a choice value and its contents.
-
+        Прочитайте значення вибору та його вміст.
     Args:
     ----
         buffer: Buffer of bytes to read from.
-        choices: The available choices.
+        choices: The available choices.(Доступні варіанти)
 
     Returns:
     -------
@@ -254,19 +265,19 @@ def decode_oid(value: bytes) -> str:
 
 def read_tlv_oid(buffer: bytes) -> tuple[bytes, str]:
     """Read an encoded object identifier.
-
+        Прочитати закодований ідентифікатор об’єкта
     Args:
     ----
         buffer: Buffer of bytes to read from.
-
+        Буфер байтів для читання
     Raises:
     ------
         ValueError: If value is not an object identifier.
-
+        Якщо значення не є ідентифікатором об'єкта
     Returns:
     -------
         The remaining (unread) buffer, the decoded oid.
-
+        Залишковий (непрочитаний) буфер, декодований oid.
     """
     buffer, data_type, value = decode_tlv(buffer)
     if data_type != Asn1DataType.OBJECT_IDENTIFIER or value is None:
@@ -277,13 +288,13 @@ def read_tlv_oid(buffer: bytes) -> tuple[bytes, str]:
 
 
 def encode_length(value: int) -> bytes:
-    """Encode length for BER."""
+    """Encode length for BER. Довжина кодування для BER"""
     # If length fits in 7 bits, we can encode it directly
     if value <= 0xEF:
         return value.to_bytes(1, "big")
-    # Otherwise we encode it like an integer
+    # Otherwise we encode it like an integer(В іншому випадку ми кодуємо його як ціле число)
     enc = encode_lv_int(value)
-    # We need to set the first bit to 1
+    # We need to set the first bit to 1(Нам потрібно встановити перший біт на 1)
     enc = bytes(enc[0] | 0b1000_0000) + enc[1:]
     return enc
 
@@ -371,13 +382,13 @@ def encode_tlv_value(data_type: Asn1DataType, value: int | str | bytes) -> bytes
 
 
 def encode_tlv_choice(value: IntEnum, content: bytes) -> bytes:
-    """Encode a choice with contents."""
+    """Encode a choice with contents.(Закодуйте вибір вмістом)"""
     return bytes([0b1010_0000 | value]) + encode_length(len(content)) + content
 
 
 class PDUType(IntEnum):
     """SNMP request type."""
-
+"""Перелік, що визначає типи SNMP PDU """
     GetRequest = 0
     GetNextRequest = 1
     GetResponse = 2
@@ -387,7 +398,8 @@ class PDUType(IntEnum):
 @dataclass
 class SNMPMessage:
     """SNMP message object."""
-
+"""Клас, який представляє SNMP повідомлення(містить інформацію про це повідомлення). Містить версію, спільноту, тип PDU"""
+"""ID запиту, статус помилки, індекс помилки та зв'язки змінних."""
     version: int
     community: str
     pdu_type: PDUType
@@ -403,11 +415,11 @@ class SNMPMessage:
         Args:
         ----
             buffer: The bytes to read from.
-
+                    Байти для читання.
         Returns:
         -------
             The constructed message, the remaining (unread) buffer.
-
+            Сконструйоване повідомлення, залишився (непрочитаний) буфер.
         """
         remaining, buffer = read_tlv_sequence(buffer)
 
@@ -466,13 +478,16 @@ class SNMPMessage:
 
         Variable bindings (in case of a GetRequest message) need to be changed
         afterwards by manually changing self.variable_bindings.
-
+        
+        Необхідно змінити прив’язки змінних (у разі повідомлення GetRequest).
+        згодом, вручну змінивши self.variable_bindings.
+        
         error_status and error_index are not changed.
 
         Returns
         -------
             A new SNMPMessage object containing copied values from self.
-
+            Новий об’єкт SNMPMessage, що містить скопійовані значення з себе.
         """
         match self.pdu_type:
             case PDUType.GetRequest | PDUType.SetRequest | PDUType.GetNextRequest:
@@ -492,7 +507,7 @@ class SNMPMessage:
         )
 
 
-# Setup managed objects
+# Setup managed objects(Налаштувати керовані об'єкти)
 MANAGED_OBJECTS = {
     # Entered
     "1.3.6.1.3.1.0": Object(
@@ -510,6 +525,7 @@ MANAGED_OBJECTS = {
 
 
 def get_next(oid: str) -> tuple[str, Object] | tuple[None, None]:
+    """Функція для отримання наступного доступного об'єкта у MIB (Management Information Base) за заданим OID."""
     """Get next accessible element in the MIB for a given OID.
 
     Args:
@@ -519,7 +535,7 @@ def get_next(oid: str) -> tuple[str, Object] | tuple[None, None]:
     Returns:
     -------
         The next OID and the next managed object in the MIB or None if none exists.
-
+            Наступний OID і наступний керований об’єкт у MIB або None, якщо такого не існує.
     """
     # Sort objects by OID lexicographically
     objects = sorted(MANAGED_OBJECTS.items(), key=lambda t: t[0])
@@ -536,16 +552,17 @@ def get_next(oid: str) -> tuple[str, Object] | tuple[None, None]:
 
 
 def handle_snmp_message(message: SNMPMessage) -> SNMPMessage:
+    """Функція для обробки вхідного SNMP повідомлення та побудови відповіді."""
     """Construct response message and handle possible action.
 
     Args:
     ----
         message: The incoming SNMP request message.
-
+                    Вхідне повідомлення запиту SNMP.
     Returns:
     -------
         The response SNMP message.
-
+        Відповідне повідомлення SNMP.
     """
     response = message.response()
     match message.pdu_type:
@@ -559,22 +576,22 @@ def handle_snmp_message(message: SNMPMessage) -> SNMPMessage:
                     not (obj := MANAGED_OBJECTS.get(var.oid, None))
                     or obj.max_access == MaxAccess.not_accessible
                 ):
-                    # OID does not exist or is not accessible
-                    # Set error status and index
+                    # OID does not exist or is not accessible(OID не існує або недоступний)
+                    # Set error status and index(Встановити статус помилки та індекс)
                     response.error_index = idx
                     response.error_status = ErrorStatus.noSuchName
-                    # Return otherwise identical message
+                    # Return otherwise identical message(Повернути в іншому випадку ідентичне повідомлення)
                     return response
-                # Update variable binding with type and value
+                # Update variable binding with type and value(Оновіть зв’язування змінної за допомогою типу та значення)
                 new_var_bindings.append(
                     VariableBind(var.oid, obj.data_type, obj.value),
                 )
-            # Update the variable bindings
+            # Update the variable bindings(Оновіть прив’язки змінних)
             response.variable_bindings = new_var_bindings
         case PDUType.SetRequest:
-            # First check if all variable bindings are valid
+            # First check if all variable bindings are valid(Спочатку перевірте, чи всі прив’язки змінних дійсні)
             for idx, var in enumerate(message.variable_bindings):
-                # Check if OID exists and we have write access
+                # Check if OID exists and we have write access(Перевірте, чи існує OID і чи є у нас доступ для запису)
                 if (
                     not (obj := MANAGED_OBJECTS.get(var.oid, None))
                     or obj.max_access != MaxAccess.read_write
@@ -582,66 +599,66 @@ def handle_snmp_message(message: SNMPMessage) -> SNMPMessage:
                     response.error_index = idx
                     response.error_status = ErrorStatus.noSuchName
                     return response
-                # Check if type is compatible
+                # Check if type is compatible(Перевірте, чи тип сумісний)
                 if obj.data_type != var.data_type:
                     response.error_index = idx
                     response.error_status = ErrorStatus.badValue
                     return response
-            # Only after making sure all are valid, set the values
+            # Only after making sure all are valid, set the values(Лише переконавшись, що всі дійсні, установіть значення)
             for var in message.variable_bindings:
                 # Set the new value
                 MANAGED_OBJECTS[var.oid].value = var.value
-                # We don't need to update the variable bindings here, because the value is
-                # set to the value contained in the incoming request.
+                # We don't need to update the variable bindings here, because the value is(Тут нам не потрібно оновлювати прив’язки змінних, оскільки значення є)
+                # set to the value contained in the incoming request.(додайте до значення, яке міститься у вхідному запиті.)
         case PDUType.GetNextRequest:
             new_var_bindings = []
             for idx, var in enumerate(message.variable_bindings):
-                # Find the next OID in the objects map
+                # Find the next OID in the objects map(Знайдіть наступний OID на карті об’єктів)
                 next_oid, next_object = get_next(var.oid)
-                # If no new next element exists, we set the error status
+                # If no new next element exists, we set the error status(Якщо новий наступний елемент не існує, ми встановлюємо статус помилки)
                 if next_oid is None or next_object is None:
                     response.error_index = idx
                     response.error_status = ErrorStatus.noSuchName
                     # Return otherwise identical message
                     return response
-                # Update variable binding with type and value
+                # Update variable binding with type and value(Оновіть зв’язування змінної за допомогою типу та значення)
                 new_var_bindings.append(
                     VariableBind(next_oid, next_object.data_type, next_object.value),
                 )
-            # Update the variable bindings
+            # Update the variable bindings(Оновіть прив’язки змінних)
             response.variable_bindings = new_var_bindings
     return response
 
 
 def main(args_: list[str]) -> None:
     """Main function."""
-    # Parse command line arguments
+    # Parse command line arguments(Аналіз аргументів командного рядка)
     parser = ArgumentParser()
     parser.add_argument("-p", "--port", default=161, type=int)
     args = parser.parse_args(args_)
     port = args.port
     host = "127.0.0.1"
-    # Setup socket
+    # Setup socket(налаштування)
     bufsize = 2048
     sock = socket.socket(type=socket.SOCK_DGRAM)
     sock.bind((host, port))
-    # Add device IP to managed objects
+    # Add device IP to managed objects(Додайте IP пристрою до керованих об’єктів)
     MANAGED_OBJECTS["1.3.6.1.3.3.0"] = Object(
         Asn1DataType.IpAddress,
         host,
         max_access=MaxAccess.read_write,
     )
-    # Read-write loop
+    # Read-write loop(Цикл читання-запису)
     while True:
-        # Receive message
+        # Receive message(Отримати повідомлення)
         message, addr = sock.recvfrom(bufsize)
         print(f"Received message from {addr} ({len(message)}): {message.hex(' ', 1)}")
-        # Parse SNMP message into object
+        # Parse SNMP message into object(Розібрати повідомлення SNMP в об’єкт)
         snmp_message, _ = SNMPMessage.from_bytes(message)
         print(snmp_message)
-        # Construct response and handle possible action (SetRequest)
+        # Construct response and handle possible action (SetRequest)(Створення відповіді та обробка можливих дій (SetRequest))
         response = handle_snmp_message(snmp_message)
-        # Send response back
+        # Send response back(Надіслати відповідь назад)
         print(f"Response is {response}")
         sock.sendto(response.encode_tlv(), addr)
 
